9b286a3b0572a6f08f56889e807958a0eb2efbc5
Altazitaz <vivetalexandra@gmail.com>
2023-10-04 20:34:19 +0200

    récupération chat en prod dans responsive test

--- adoption/index.php
+++ adoption/index.php
@@ -10,34 +10,7 @@ include('../inc/header.php');
 	</div>
 
 	<div class="gridywrap">
-		<div class="gridy-2 gridyhe-1">
-			<div class="gridimg" style="background-image: url(../assets/img/adoption/skyR.jpg)">&nbsp;</div>
-			
-			<div class="gridinfo">
-			<h3>Sky</h3>
-			<div class="gridmeta">
-			<p class="gridwhen"><i class="fa-solid fa-paw"></i>Né le 15/05/2021, Européenne bleue non LOOF</p>
-				<p class="gridwho"><i class="fa-solid fa-briefcase-medical"></i> Pucée, vaccinée, testée FIV/FELV négative, stérilisée, déparasitée int/ext</p>
-			</div>
-			<p class="gridexerpt">Sky est une minette hyper active qui est extrêmement pot de colle. Elle aime avoir de l'attention par des jeux, des carresses et calins. Si vous ne vous occupez pas d'elle, elle grimpera sur vos épaules pour vous le faire comprendre. Elle s'entend avec chiens et chats mais elle pourra être placée toute seule si sa future famille est très présente au domicile et a réellement du temps à lui consacrer. C'est une minette en or qui a beaucoup à apporter.</p>
-			<a href="https://www.facebook.com/Association-HandiCats-Adoptions-681342371878793/photos/a.681720511840979/5262889450390706" target="_blank" class="grid-btn grid-more"><span>Me découvrir </span> <i class="fa fa-plus"></i></a>
-			</div>
-		</div>
-
-		<div class="gridy-1 gridyhe-1">
-			<div class="gridimg" style="background-image: url(../assets/img/adoption/aegonR.jpg)">&nbsp;</div>
-				
-			<div class="gridinfo">
-			<h3>Aegon</h3>
-			<div class="gridmeta">
-				<p class="gridwhen"><i class="fa-solid fa-paw"></i>Né le 01/09/2017, Européen noir et blanc non LOOF</p>
-				<p class="gridwho"><i class="fa-solid fa-briefcase-medical"></i> Pucé, vacciné, testé FIV/FELV négatif, castré, déparasité int/ext</p>
-			</div>
-			<p class="gridexerpt">Aegon est un petit chat très sage et sociable. Gentil avec tout le monde; il aime la présence de ses congénères. Très joueur il a besoin de la compagnie d'autres chats. Il lui faudra au moins un copain chat sympa dans sa future famille. Il s'entend aussi très bien avec les chiens.</p>
-			<a href="https://handicats.forumgratuit.org/t5879-aegon-annonce-type-pour-sites-gratuits" target="_blank" class="grid-btn grid-more"><span>Me découvrir </span> <i class="fa fa-plus"></i></a>
-			</div>
-		</div>
-
+		
 		<div class="gridy-1 gridyhe-2 ">
 			<div class="gridimg" style="background-image: url(../assets/img/adoption/hypnose.jpg)">&nbsp;</div>
 			
@@ -91,43 +64,43 @@ include('../inc/header.php');
 		</div>
 
 		<div class="gridy-1 gridyhe-2 ">
-			<div class="gridimg" style="background-image: url(../assets/img/adoption/siki.jpg)">&nbsp;</div>
+			<div class="gridimg" style="background-image: url(../assets/img/adoption/parme.jpg)">&nbsp;</div>
 			
 			<div class="gridinfo verticale">
-			<h3>Siki</h3>
+			<h3>Parme</h3>
 			<div class="gridmeta">
-				<p class="gridwhen"><i class="fa-solid fa-paw"></i>Née le 25/10/2021, Européen tricolore non LOOF</p>
-				<p class="gridwho"><i class="fa-solid fa-briefcase-medical"></i> Pucée, vaccinée, testée FIV/FELV négative, castrée, déparasitée int/ext</p>
+				<p class="gridwhen"><i class="fa-solid fa-paw"></i>Née le 05/03/2020, Red point à poils longs non LOOF</p>
+				<p class="gridwho"><i class="fa-solid fa-briefcase-medical"></i> Pucée, vaccinée, testée FIV positive/FELV négative, stérilisée, déparasitée int/ext</p>
 			</div>
-			<p class="gridexerpt verticale">Siki est une petite minette très sociable et câline qui aime la présence de ses congénères. Née avec une malformation des pattes avant qui ne l'empêche pas de vivre pratiquement normalement, elle aura néamoins besoin de légers aménagements. Elle ne devra notamment pas pouvoir grimper trop haut pour ne pas avoir à se réceptionner sur ses pattes avant.  Bien sûr, les sorties en extérieur sont totalement proscrites ou uniquement en enclos extérieur totalement sécurisé pour sa sécurité. Siki est très sociable et câline, elle aime les autres chats et n'a pas peur des chiens, elle devra être placée obligatoirement avec au moins un autre chat sociable.</p>
-			<a href="https://www.facebook.com/Association-HandiCats-Adoptions-681342371878793/photos/a.681720511840979/5262890323723952" target="_blank" class="grid-btn grid-more"><span>Me découvrir</span> <i class="fa fa-plus"></i></a>
+			<p class="gridexerpt verticale">Parme est une magnifique minette très sociable qui vient de Guadeloupe. Elle est handicapée de naissance d'une patte avant mais également borgne. Elle s'entend bien avec les autres chats mais n'a pas obligatoirement besoin de leur présence. Testée FIV+, cela ne l'empêche absolument pas de vivre normalement, avec d'autres chats stérilisés et pacifiques non dominants comme elle. Avec les humains, elle pourra être timide mais avec du temps et de la confiance, les câlins et les demandes d'attention arriveront. C'est une minette très sensible et adorable qui s’épanouira dans un environnement calme sans jeunes enfants.</p>
+			<a href="https://www.facebook.com/Association-HandiCats-Adoptions-681342371878793/photos/a.681720511840979/5930467443632900" target="_blank" class="grid-btn grid-more"><span>Me découvrir</span> <i class="fa fa-plus"></i></a>
 			</div>
 		</div>
 
 		<div class="gridy-1 gridyhe-1">
-			<div class="gridimg" style="background-image: url(../assets/img/adoption/xena.jpg)">&nbsp;</div>
+			<div class="gridimg" style="background-image: url(../assets/img/adoption/sueno.jpg)">&nbsp;</div>
 			<div class="gridinfo">
-			<h3>Xéna</h3>
+			<h3>Sueño</h3>
 			<div class="gridmeta">
-				<p class="gridwhen"><i class="fa-solid fa-paw"></i>Née le 15/07/2021, Européen tigrée et blanc non LOOF</p>
-				<p class="gridwho"><i class="fa-solid fa-briefcase-medical"></i> Pucée, vaccinée, testée FIV/FELV négative, castrée, déparasitée int/ext</p>
+				<p class="gridwhen"><i class="fa-solid fa-paw"></i>Né le 15/09/2022, Européen marbré marron non LOOF</p>
+				<p class="gridwho"><i class="fa-solid fa-briefcase-medical"></i> Pucé, vacciné TCL, testé FIV/FELV négatif, déparasité int/ext</p>
 			</div>
-			<p class="gridexerpt"> Xéna peut être craintive au premier abord mais une fois la confiance établie, c'est une petite minette curieuse et très gentille qui saura apprécier les calins et caresses. Très gourmande et joueuse, Xéna est habituée à vivre avec chien et chats et elle aura besoin d'au moins un autre chat sociable dans sa future maison.</p>
-			<a href="https://www.facebook.com/Association-HandiCats-Adoptions-681342371878793/photos/a.681720511840979/5055887194424267"  target="_blank" class="grid-btn grid-more"><span>Me découvrir</span> <i class="fa fa-plus"></i></a>
+			<p class="gridexerpt"> Sueño est un beau minet né d'une portée de 5 loulous. Ce sont des chatons en pleine forme qui n'ont aucun handicap. Comme tout chaton, il est très joueur, énergique et pourrait faire quelques petites bêtises ! Placement obligatoire avec un autre chat déjà présent au foyer ou à 2 avec un de ses frères et soeur.</p>
+			<a href="https://www.facebook.com/photo/?fbid=6091867147492928&set=a.681720511840979"  target="_blank" class="grid-btn grid-more"><span>Me découvrir</span> <i class="fa fa-plus"></i></a>
 			</div>
 		</div>
 
 		<div class="gridy-1 gridyhe-1">
-			<div class="gridimg" style="background-image: url(../assets/img/adoption/astree.jpg)">&nbsp;</div>
-			
+			<div class="gridimg" style="background-image: url(../assets/img/adoption/aegonR.jpg)">&nbsp;</div>
+				
 			<div class="gridinfo">
-			<h3>Astrée</h3>
+			<h3>Aegon</h3>
 			<div class="gridmeta">
-				<p class="gridwhen"><i class="fa-solid fa-paw"></i>Née le 01/01/2015, Européen tigrée marron avec une tache blanche au plastron non LOOF</p>
-				<p class="gridwho"><i class="fa-solid fa-briefcase-medical"></i> Pucée, vaccinée, testée FIV/FELV négative, stérilisée, déparasitée int/ext</p>
+				<p class="gridwhen"><i class="fa-solid fa-paw"></i>Né le 01/09/2017, Européen noir et blanc non LOOF</p>
+				<p class="gridwho"><i class="fa-solid fa-briefcase-medical"></i> Pucé, vacciné, testé FIV/FELV négatif, castré, déparasité int/ext</p>
 			</div>
-			<p class="gridexerpt">Astrée est une minette de caractère mais très attachante. Très caline et douce, elle peut passer des heures à se faire caresser, gratouiller... Elle a des immenses quarts d’heure de folie le soir, où elle sprinte dans tous les sens. C’est également une grande joueuse. Assez exclusive avec son humaine, un placement sans chat sera donc préférable.</p>
-			<a href="https://www.facebook.com/Association-HandiCats-Adoptions-681342371878793/photos/a.681720511840979/4704372376242419" target="_blank" class="grid-btn grid-more"><span>Me découvrir</span> <i class="fa fa-plus"></i></a>
+			<p class="gridexerpt">Aegon est un petit chat très sage et sociable. Gentil avec tout le monde; il aime la présence de ses congénères. Très joueur il a besoin de la compagnie d'autres chats. Il lui faudra au moins un copain chat sympa dans sa future famille. Il s'entend aussi très bien avec les chiens.</p>
+			<a href="https://handicats.forumgratuit.org/t5879-aegon-annonce-type-pour-sites-gratuits" target="_blank" class="grid-btn grid-more"><span>Me découvrir </span> <i class="fa fa-plus"></i></a>
 			</div>
 		</div>
 
@@ -159,31 +132,17 @@ include('../inc/header.php');
 			</div>
 		</div>			
 
-		<!-- <div class="gridy-1 gridyhe-1">
-			<div class="gridimg" style="background-image: url(../assets/img/adoption/antman.jpg)">&nbsp;</div>
-				
-			<div class="gridinfo">
-			<h3>Antman</h3>
-			<div class="gridmeta">
-				<p class="gridwhen"><i class="fa-solid fa-paw"></i>Né le 20/07/2021, marbré marron non LOOF</p>
-				<p class="gridwho"><i class="fa-solid fa-briefcase-medical"></i> Pucé, vacciné, testé FIV/FELV négatif, castré, déparasité int/ext</p>
-			</div>
-			<p class="gridexerpt"> C’est un petit gabarit, calme la plupart du temps mais il ne faut pas se fier à son air innocent : comme tout chaton, les bêtises, c'est sa passion ! Très sociable avec les chiens, les chats, il est également habitué aux enfants. Antman devra trouver un foyer avec déjà un chat adulte sociable ou alors être adopté en duo avec un autre Handi'cats.</p>
-			<a href="https://www.facebook.com/Association-HandiCats-Adoptions-681342371878793/photos/a.681720511840979/4752585128087810" target="_blank" class="grid-btn grid-more"><span>Me découvrir </span> <i class="fa fa-plus"></i></a>
-			</div>
-		</div> -->
-
 		<div class="gridy-1 gridyhe-1">
-			<div class="gridimg" style="background-image: url(../assets/img/adoption/rosaceR.jpg)">&nbsp;</div>
-				
+			<div class="gridimg" style="background-image: url(../assets/img/adoption/maya.jpg)">&nbsp;</div>
+			
 			<div class="gridinfo">
-			<h3>Rosace</h3>
+			<h3>Maya</h3>
 			<div class="gridmeta">
-				<p class="gridwhen"><i class="fa-solid fa-paw"></i>Née le 01/01/2014, Européen noire non LOOF</p>
-				<p class="gridwho"><i class="fa-solid fa-briefcase-medical"></i> Pucée, vaccinée, testée FIV/FELV négative, stérilisée, déparasitée int/ext</p>
+				<p class="gridwhen"><i class="fa-solid fa-paw"></i>Née le 25/04/2022, Européen tigrée non LOOF</p>
+				<p class="gridwho"><i class="fa-solid fa-briefcase-medical"></i> Pucée, vaccinée, déparasitée int/ext</p>
 			</div>
-			<p class="gridexerpt"> Rosace est une jolie perle noire qui a la particularité d'être un vériable chat-chien. Très calme, elle adore suivre son humain et s’intéresser à ce qu’il fait, surtout si c’est préparer à manger ! C’est un chat qui aime passer son temps couché près de son humain et la solitude lui pèse même si elle est capable de la supporter donc une personne présente serait l’idéal.</p>
-			<a href="https://www.facebook.com/Association-HandiCats-Adoptions-681342371878793/photos/5630601200286194" target="_blank" class="grid-btn grid-more"><span>Me découvrir </span> <i class="fa fa-plus"></i></a>
+			<p class="gridexerpt">Arrivée avec sa maman et ses 2 frères, Maya est une petite chipie qui reste trouillarde au premier abord mais fait très vite des câlins et réclame des papouilles. Elle a une petite particularité : les paupières de ses yeux sont tombantes ce qui lui donne un charme fou et elle a des chaussettes blanche seulement a l'arrière. A adopter par 2 ou avec au moins un autre chat dans son futur foyer.</p>
+			<a href="https://www.facebook.com/Association-HandiCats-Adoptions-681342371878793/photos/pcb.5690016711011309/5690011987678448/" target="_blank" class="grid-btn grid-more"><span>Me découvrir</span> <i class="fa fa-plus"></i></a>
 			</div>
 		</div>
 
@@ -225,7 +184,7 @@ include('../inc/header.php');
 				<p class="gridwho"><i class="fa-solid fa-briefcase-medical"></i> Pucée, vaccinée, testée FIV/FELV négative, stérilisée, déparasitée int/ext</p>
 			</div>
 			<p class="gridexerpt verticale">Jinx est une jolie petite minette sociable et hyperactive. Trouvée errante avec de forts troubles neurologiques, il s'est avéré qu'elle été atteinte d'otites profondes qui bouchaient totalement ses 2 conduits auditifs. Après un long traitement, Jinx a beaucoup récupéré mais elle reste handicapée. Elle est aujourd'hui ataxique (elle a de légères pertes d'équilibre et une démarche chaloupée) et totalement sourde. Cela ne l'empêche pas de vivre sa vie et d'être une petite chipie qui, comme tout chaton, aime faire des bêtises. Très joueuse elle aura besoin d'un autre chat qui saura terminer son éducation. Il faudra aménager un petit peu son environnement et faire attention qu'elle ne puisse pas grimper trop haut car elle pourrait en tomber ou ne pas savoir en redescendre vu qu’elle est très fofolle et kamikaze. Si une tornade pour pimenter et égayer votre quotidien ne vous fait pas peur, c’est elle qu’il vous faut !</p>
-			<a href="https://www.facebook.com/Association-HandiCats-Adoptions-681342371878793/photos/5281699218509729" target="_blank" class="grid-btn grid-more"><span>Me découvrir</span> <i class="fa fa-plus"></i></a>
+			<a href="https://www.facebook.com/photo.php?fbid=590247486462808&set=pb.100064326863857.-2207520000.&type=3" target="_blank" class="grid-btn grid-more"><span>Me découvrir</span> <i class="fa fa-plus"></i></a>
 			</div>
 		</div>
 
@@ -245,7 +204,7 @@ include('../inc/header.php');
 			</div>
 		</div>
 
-		<div class="gridy-1 gridyhe-1">
+		<!-- <div class="gridy-1 gridyhe-1">
 			<div class="gridimg" style="background-image: url(../assets/img/adoption/syrahR.jpg)">&nbsp;</div>
 			<div class="gridinfo">
 			<h3>Syrah</h3>
@@ -256,7 +215,7 @@ include('../inc/header.php');
 			<p class="gridexerpt"> Syrah est une vraie coquine, très joueuse et toujours joyeuse, elle peut avoir peur de l'inconnu au départ mais elle se transforme très vite en machine a ronrons malgré ce qu'elle a vécu. A adopter par 2 ou avec au moins un autre chat dans son futur foyer.</p>
 			<a href="https://www.facebook.com/Association-HandiCats-Adoptions-681342371878793/photos/pcb.5699750146704632/5699749556704691/"  target="_blank" class="grid-btn grid-more"><span>Me découvrir</span> <i class="fa fa-plus"></i></a>
 			</div>
-		</div>
+		</div> -->
 
 		<div class="gridy-1 gridyhe-1">
 			<div class="gridimg" style="background-image: url(../assets/img/adoption/iraisRtest.jpg)">&nbsp;</div>
@@ -287,18 +246,34 @@ include('../inc/header.php');
 		</div>
 
 		<div class="gridy-1 gridyhe-1">
-			<div class="gridimg" style="background-image: url(../assets/img/adoption/mayaR.jpg)">&nbsp;</div>
+			<div class="gridimg" style="background-image: url(../assets/img/adoption/feline.jpg)">&nbsp;</div>
 			
 			<div class="gridinfo">
-			<h3>Maya</h3>
+			<h3>Féline</h3>
 			<div class="gridmeta">
-				<p class="gridwhen"><i class="fa-solid fa-paw"></i>Née le 25/04/2022, Européen tigrée non LOOF</p>
-				<p class="gridwho"><i class="fa-solid fa-briefcase-medical"></i> Pucée, vaccinée, déparasitée int/ext</p>
+				<p class="gridwhen"><i class="fa-solid fa-paw"></i>Née le 15/06/2020, Européen noire et blanche non LOOF</p>
+				<p class="gridwho"><i class="fa-solid fa-briefcase-medical"></i> Pucée, stérilisée, vaccinée TCL, testée calicivirus/FIV/FELV négative, déparasitée int/ext</p>
 			</div>
-			<p class="gridexerpt">Arrivée avec sa maman et ses 2 frères, Maya est une petite chipie qui reste trouillarde au premier abord mais fait très vite des câlins et réclame des papouilles. Elle a une petite particularité : les paupières de ses yeux sont tombantes ce qui lui donne un charme fou et elle a des chaussettes blanche seulement a l'arrière. A adopter par 2 ou avec au moins un autre chat dans son futur foyer.</p>
-			<a href="https://www.facebook.com/Association-HandiCats-Adoptions-681342371878793/photos/pcb.5690016711011309/5690011987678448/" target="_blank" class="grid-btn grid-more"><span>Me découvrir</span> <i class="fa fa-plus"></i></a>
+			<p class="gridexerpt">Féline est une petite minette très timide. Elle a une malformation congénitale aux 2 pattes avant, non opérable, elles peuvent continuer à se redresser mais restera probablement malformées à vie. Actuellement elle peut grimper mais pas redescendre sur une grande hauteur et aura besoin de surveillance. Elle aura besoin d’une famille patiente, douce et calme. </p>
+			<a href="https://www.facebook.com/681342371878793/photos/pb.100064326863857.-2207520000./4550145848331740/?type=3" target="_blank" class="grid-btn grid-more"><span>Me découvrir</span> <i class="fa fa-plus"></i></a>
 			</div>
 		</div>
+
+		<div class="gridy-1 gridyhe-1">
+			<div class="gridimg" style="background-image: url(../assets/img/adoption/smoke.jpg)">&nbsp;</div>
+			
+			<div class="gridinfo">
+			<h3>Smoke</h3>
+			<div class="gridmeta">
+				<p class="gridwhen"><i class="fa-solid fa-paw"></i>Né le 15/06/2020, Européen gris bleu non LOOF</p>
+				<p class="gridwho"><i class="fa-solid fa-briefcase-medical"></i> Pucée, stérilisée, vaccinée TCL, testée calicivirus/FIV/FELV négative, déparasitée int/ext</p>
+			</div>
+			<p class="gridexerpt">Smoke est un petit minet très timide. Il a une malformation congénitale aux 2 pattes avant, non opérable, elles peuvent continuer à se redresser mais restera probablement malformées à vie. Actuellement il peut grimper mais pas redescendre sur une grande hauteur et aura besoin de surveillance. Il sera à adopter avec sa soeur Féline dans une famille calme. </p>
+			<a href="https://www.facebook.com/681342371878793/photos/pb.100064326863857.-2207520000./4550145848331740/?type=3" target="_blank" class="grid-btn grid-more"><span>Me découvrir</span> <i class="fa fa-plus"></i></a>
+			</div>
+		</div>
+
+		
 	</div>
 	<div class="ghost">
 		<p>texte d'aide</p>
